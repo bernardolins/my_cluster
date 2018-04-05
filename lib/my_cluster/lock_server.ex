7cbf876e9872a12590f10320aea576e7d69638b4
Vinicius Souza <vinicius@codemakers.com.br>
2018-04-05 18:49:34 -0300

    handle call and test topology changes

--- lib/my_cluster/lock_server.ex
+++ lib/my_cluster/lock_server.ex
@@ -1,14 +1,20 @@
 defmodule LockServer do
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, nil)
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
   end
 
-  def init(_) do
-    {:ok, nil}
+  def init(state) do
+    {:ok, state}
   end
 
   def handle_cast(:work, state) do
     IO.puts("Working #{Node.self()}")
-    {:noreply, state}
+    {:noreply, "CHANGED BITCH!"}
+  end
+
+  def handle_call(:work, _, state) do
+    IO.puts("Call Working #{Node.self()}")
+    IO.puts(state)
+    {:reply, [Node.self(), state], state}
   end
 end
