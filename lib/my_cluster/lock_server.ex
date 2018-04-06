feb4e869bf6a59019ec00c41e1597ac55d759c3f
bernardolins <bernardo.lins@corp.globo.com>
2018-04-06 08:39:17 -0300

    Add begin and end handoff callbacks

--- lib/my_cluster/lock_server.ex
+++ lib/my_cluster/lock_server.ex
@@ -7,6 +7,16 @@ defmodule LockServer do
     {:ok, state}
   end
 
+  def handle_call({:swarm, :begin_handoff}, _from, state) do
+    IO.puts "BEGIN HANDOFF"
+    {:reply, {:resume, state}, state}
+  end
+
+  def handle_cast({:swarm, :end_handoff, state}, _state) do
+    IO.puts "END HANDOFF"
+    {:noreply, state}
+  end
+
   def handle_cast(:work, state) do
     IO.puts("Working #{Node.self()}")
     {:noreply, "CHANGED BITCH!"}
